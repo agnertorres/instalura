87b74361a50d191b7e7c5daefb43836f70f709f3
Agner Torres <agnertorres@gmail.com>
2018-08-21 18:08:39 -0300

    implementação da autenticação

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,13 +1,51 @@
 import React, { Component } from 'react'
 
 export default class Login extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      msg: ''
+    }
+
+  }
+
+  sendLoginForm(event) {
+    event.preventDefault()
+
+    const requestInfo = {
+      method: 'POST',
+      body: JSON.stringify({
+        login: this.login.value,
+        senha: this.password.value
+      }),
+      headers: new Headers({
+        'Content-type': 'application/json'
+      })
+    }
+
+    fetch('https://instalura-api.herokuapp.com/api/public/login', requestInfo)
+      .then( response => {
+        if (response.ok) {
+          return response.text()
+        } else {
+          this.setState({
+            msg: 'Não foi possível fazer login'
+          })
+        }
+      })
+      .then( token => {
+        console.log(token)
+      })
+  }
+
   render() {
     return (
       <div className="login-box">
         <h1 className="header-logo">Instalura</h1>
-        <form>
-          <input type="text" />
-          <input type="password" />
+        <form onSubmit={ this.sendLoginForm.bind(this) }>
+          <input type="text" ref={ input => this.login = input } />
+          <input type="password" ref={ input => this.password = input } />
           <input type="submit" value="login" />
         </form>
       </div>
