4e49700e6c64ba73fee5309fe304857d0112969a
Agner Torres <agnertorres@gmail.com>
2018-09-27 18:26:14 -0300

    Movendo as regras de negócio dos componentes de foto para o componente de timeline

--- src/components/Photo.js
+++ src/components/Photo.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
-import Pubsub from 'pubsub-js'
 
 class PhotoHeader extends Component {
   render() {
@@ -21,66 +20,17 @@ class PhotoHeader extends Component {
 }
 
 class PhotoInfo extends Component {
-
-  constructor( props ) {
-    super(props)
-
-    this.state = {
-      likers: this.props.photo.likers,
-      comments: this.props.photo.comentarios
-    }
-  }
-
-  componentWillMount() {
-    Pubsub.subscribe('update-liker', (topic, infoLiker) => {
-
-      if (this.props.photo.id === infoLiker.photoId) {
-
-        const possibleLiker = this.state.likers.find( liker => {
-          return liker.login === infoLiker.liker.login 
-        })
-
-        if (possibleLiker === undefined) {
-          
-          const newLikers = this.state.likers.concat(infoLiker.liker)
-          
-          this.setState({
-            likers: newLikers
-          })
-        } else {
-          const newLikers = this.state.likers.filter( liker => { 
-            return liker.login !== infoLiker.liker.login 
-          })
-
-          this.setState({
-            likers: newLikers
-          })
-        }
-      }
-    })
-
-    Pubsub.subscribe('new-comments', (topic, infoComment) => {
-
-      if (this.props.photo.id === infoComment.photoId) {
-        const newComments = this.state.comments.concat(infoComment.newComment)
-
-        this.setState({
-          comments: newComments
-        })
-      }
-    })
-  }
-
+  
   render() {
     return (
       <div className="foto-info">
         <div className="foto-info-likes">
           {
-            this.state.likers.map(( liker ) => {
+            this.props.photo.likers.map(( liker ) => {
               return  <Link to={`/timeline/${ liker.login }`} key={ liker.login } >{ liker.login }, </Link>
             })
           }
-          <span className={ this.state.likers.length > 0 ? '' : 'hide' }>curtiram</span>
+          <span className={ this.props.photo.likers.length > 0 ? '' : 'hide' }>curtiram</span>
         </div>
 
         <p className="foto-info-legenda">
@@ -90,7 +40,7 @@ class PhotoInfo extends Component {
 
         <ul className="foto-info-comentarios">
           {
-            this.state.comments.map(( comment ) => {
+            this.props.photo.comentarios.map(( comment ) => {
               return (
                 <li key={ comment.id } className="comentario">
                   <Link to={`/timeline/${ comment.login }`} className="foto-info-autor">{ comment.login }</Link>
@@ -107,70 +57,20 @@ class PhotoInfo extends Component {
 
 class PhotoUpdates extends Component {
 
-  constructor( props ) {
-    super(props)
-
-    this.state = {
-      liked: this.props.photo.likeada
-    }
-  }
-
   likePhoto( event ) {
     event.preventDefault()
-
-    const authToken = localStorage.getItem('auth-token')
-
-    const requestUrl = `https://instalura-api.herokuapp.com/api/fotos/${ this.props.photo.id }/like?X-AUTH-TOKEN=${ authToken }`,
-          requestParams = { method: 'POST' }
-
-    fetch(requestUrl, requestParams)
-      .then( response => {
-        if(response.ok) {
-          return response.json()
-        } else {
-          throw new Error('Não foi possível curtir a foto')
-        }
-      })
-      .then( liker => {
-        this.setState({ liked: !this.state.liked })
-        
-        Pubsub.publish('update-liker', { photoId: this.props.photo.id, liker })
-      })
+    this.props.likePhoto(this.props.photo.id)
   }
 
   commentPhoto( event ) {
     event.preventDefault()
-
-    const authToken = localStorage.getItem('auth-token')
-
-    const requestUrl = `https://instalura-api.herokuapp.com/api/fotos/${ this.props.photo.id }/comment?X-AUTH-TOKEN=${ authToken }`,
-          requestParams = { 
-            method: 'POST',
-            body: JSON.stringify({
-              texto: this.comment.value
-            }),
-            headers: new Headers({
-              'Content-type': 'application/json'
-            })
-          }
-
-    fetch(requestUrl, requestParams)
-      .then( response => {
-        if(response.ok) {
-          return response.json()
-        } else {
-          throw new Error('Não foi possível comentar a foto')
-        }
-      })
-      .then( newComment => {
-        Pubsub.publish('new-comments', { photoId: this.props.photo.id, newComment })
-      })
+    this.props.commentPhoto(this.props.photo.id, this.comment.value)
   }
 
   render() {
     return (
       <section className="fotoAtualizacoes">
-        <a onClick={ this.likePhoto.bind(this) } className={ this.state.liked ? 'fotoAtualizacoes-like-ativo' : 'fotoAtualizacoes-like' }>Likar</a>
+        <a onClick={ this.likePhoto.bind(this) } className={ this.props.photo.likeada ? 'fotoAtualizacoes-like-ativo' : 'fotoAtualizacoes-like' }>Likar</a>
         <form className="fotoAtualizacoes-form" onSubmit={ this.commentPhoto.bind(this) }>
           <input type="text" placeholder="Adicione um comentário..." className="fotoAtualizacoes-form-campo" ref={ input => this.comment = input }/>
           <input type="submit" value="Comentar!" className="fotoAtualizacoes-form-submit"/>
@@ -188,7 +88,7 @@ export default class PhotoItem extends Component {
         <img alt="foto" className="foto-src" src={ this.props.photo.urlFoto }/>
 
         <PhotoInfo photo={ this.props.photo }/>
-        <PhotoUpdates photo={ this.props.photo }/>
+        <PhotoUpdates photo={ this.props.photo } likePhoto={ this.props.likePhoto } commentPhoto={ this.props.commentPhoto }/>
       </div>
     )
   }
--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -17,6 +17,42 @@ export default class Timeline extends Component {
     Pubsub.subscribe('timeline', (topic, photos) => {
       this.setState({ photos })
     })
+
+    Pubsub.subscribe('update-liker', (topic, infoLiker) => {
+
+      const foundPhoto = this.state.photos.find( photo => photo.id === infoLiker.photoId )
+
+      foundPhoto.likeada = !foundPhoto.likeada
+
+      const possibleLiker = foundPhoto.likers.find( liker => {
+        return liker.login === infoLiker.liker.login 
+      })
+
+      if (possibleLiker === undefined) {
+        foundPhoto.likers.push(infoLiker.liker)
+
+      } else {
+        const newLikers = foundPhoto.likers.filter( liker => { 
+          return liker.login !== infoLiker.liker.login 
+        })
+
+        foundPhoto.likers = newLikers
+      }
+
+      this.setState({
+        photos: this.state.photos
+      })
+    })
+
+    Pubsub.subscribe('new-comments', (topic, infoComment) => {
+
+      const foundPhoto = this.state.photos.find( photo => photo.id === infoComment.photoId )
+      foundPhoto.comentarios.push(infoComment.newComment)
+
+      this.setState({
+        photos: this.state.photos
+      })
+    })
   }
 
   componentDidMount() {
@@ -50,11 +86,57 @@ export default class Timeline extends Component {
       })
   }
 
+  likePhoto( photoId ) {
+    const authToken = localStorage.getItem('auth-token')
+
+    const requestUrl = `https://instalura-api.herokuapp.com/api/fotos/${ photoId }/like?X-AUTH-TOKEN=${ authToken }`,
+          requestParams = { method: 'POST' }
+
+    fetch(requestUrl, requestParams)
+      .then( response => {
+        if(response.ok) {
+          return response.json()
+        } else {
+          throw new Error('Não foi possível curtir a foto')
+        }
+      })
+      .then( liker => {
+        Pubsub.publish('update-liker', { photoId: photoId, liker })
+      })
+  }
+
+  commentPhoto(photoId, commentValue) {
+    const authToken = localStorage.getItem('auth-token')
+
+    const requestUrl = `https://instalura-api.herokuapp.com/api/fotos/${ photoId }/comment?X-AUTH-TOKEN=${ authToken }`,
+          requestParams = { 
+            method: 'POST',
+            body: JSON.stringify({
+              texto: commentValue
+            }),
+            headers: new Headers({
+              'Content-type': 'application/json'
+            })
+          }
+
+    fetch(requestUrl, requestParams)
+      .then( response => {
+        if(response.ok) {
+          return response.json()
+        } else {
+          throw new Error('Não foi possível comentar a foto')
+        }
+      })
+      .then( newComment => {
+        Pubsub.publish('new-comments', { photoId: photoId, newComment })
+      })
+  }
+
   render() {
     return (
       <div className="fotos container">
         {
-          this.state.photos.map(photo => <PhotoItem key={ photo.id } photo={ photo }/>)
+          this.state.photos.map(photo => <PhotoItem key={ photo.id } photo={ photo } likePhoto={ this.likePhoto } commentPhoto={ this.commentPhoto } />)
         }
       </div>
     )
